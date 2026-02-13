15e8ccea7bb2c7fce10b5ccbe73d4022a2009aa1
ersabajanes <eduardoruizsabajanes@gmail.com>
2026-02-13 13:14:07 +0100

    FFT convolution

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,25 @@ use complex::Complex;
 use core::f32;
 use num_traits::Zero;
 
-pub fn fft(xt: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
-    let ns = xt.len();
-    let mut xf = vec![Complex::zero(); ns];
+fn next_pow(num: usize, radix: usize) -> usize {
+    let mut foo = num - 1;
+    let mut bar = 1;
 
-    if ns == 1 {
-        xf[0] += xt[0];
-        return xf;
+    while foo > 0 {
+        foo /= radix;
+        bar *= radix;
     }
 
+    bar
+}
+
+pub fn fft(xt: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
+    let ns = xt.len();
+    if ns == 1 {
+        return xt.to_vec();
+    }
+
+    let mut xf = vec![Complex::zero(); ns];
     for i in 0..radix {
         let xt0 = Vec::from_iter(xt.iter().skip(i).step_by(radix).cloned());
         let xf0 = fft(&xt0, radix);
@@ -25,6 +35,49 @@ pub fn fft(xt: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
     }
 
     xf
+}
+
+pub fn fft_it(xt: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
+    let ns = xt.len();
+    if ns == 1 {
+        return xt.to_vec();
+    }
+
+    let mut xfa = xt.to_vec();
+    let mut xfb = vec![Complex::zero(); ns];
+
+    let mut foo;
+    let mut bar = ns;
+    while bar > 1 {
+        foo = bar;
+        bar /= radix;
+
+        for n in 0..ns {
+            xfb[(n / foo) * foo + (n % radix) * bar + (n / radix) % bar] = xfa[n];
+        }
+
+        (xfa, xfb) = (xfb, xfa);
+    }
+
+    let mut foo;
+    let mut bar = 1;
+    while bar < ns {
+        foo = bar;
+        bar *= radix;
+
+        for m in 0..ns {
+            xfb[m] = Complex::zero();
+            for i in 0..radix {
+                let m2 = (m / bar) * bar + (m % foo) + i * foo;
+                let th = -f32::consts::TAU * (m % bar) as f32 / bar as f32 * i as f32;
+                xfb[m] += Complex::new(f32::cos(th), f32::sin(th)) * xfa[m2];
+            }
+        }
+
+        (xfa, xfb) = (xfb, xfa);
+    }
+
+    xfa
 }
 
 pub fn ifft(xf: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
@@ -52,10 +105,38 @@ pub fn ifft(xf: &[Complex<f32>], radix: usize) -> Vec<Complex<f32>> {
 }
 
 pub fn fft2(xt: &[Complex<f32>], w: usize, h: usize, r: usize) -> Vec<Complex<f32>> {
+    let w2 = next_pow(w, r);
+    let h2 = next_pow(h, r);
+
+    let mut xf = vec![Complex::zero(); w2 * h2];
+
+    for i in 0..h {
+        let mut xti = xt[i * w..i * w + w].to_vec();
+        xti.resize(w2, Complex::zero());
+
+        let xfi = fft(&xti, r);
+        for j in 0..w2 {
+            xf[i * w2 + j] = xfi[j];
+        }
+    }
+
+    for j in 0..w2 {
+        let xtj = Vec::from_iter(xf.iter().skip(j).step_by(w2).cloned());
+
+        let xfj = fft(&xtj, r);
+        for i in 0..h2 {
+            xf[i * w2 + j] = xfj[i];
+        }
+    }
+
+    xf
+}
+
+pub fn ifft2(xt: &[Complex<f32>], w: usize, h: usize, r: usize) -> Vec<Complex<f32>> {
     let mut xf = vec![Complex::zero(); xt.len()];
 
     for i in 0..h {
-        let xi = fft(&xt[i * w..i * w + w], r);
+        let xi = ifft(&xt[i * w..i * w + w], r);
         for j in 0..w {
             xf[i * w + j] = xi[j];
         }
@@ -63,7 +144,7 @@ pub fn fft2(xt: &[Complex<f32>], w: usize, h: usize, r: usize) -> Vec<Complex<f3
 
     for j in 0..w {
         let xj = Vec::from_iter(xf.iter().skip(j).step_by(w).cloned());
-        let xj = fft(&xj, r);
+        let xj = ifft(&xj, r);
         for i in 0..h {
             xf[i * w + j] = xj[i];
         }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,133 @@
-use std::f32::{INFINITY, NEG_INFINITY};
-
-// use core::f32;
 use fft::{complex::Complex};
 use num_traits::Zero;
 
+fn next_pow2(num: usize) -> usize {    
+    let mut foo = num - 1;
+    let mut bar = 1;
+
+    while foo > 0 {
+        foo >>= 1;
+        bar <<= 1;
+    }
+
+    bar
+}
+
 fn main() {
-    let mut w: u32 = 0;
-    let mut h: u32 = 0;
-    let mut d: u8  = 0;
-    let mut c: u8  = 0;
-    let mut pix = qoi::load("noise_blue.qoi", &mut w, &mut h, &mut d, &mut c).unwrap();
+    // Image
+    let mut w0: u32 = 0;
+    let mut h0: u32 = 0;
+    let mut d0: u8  = 0;
+    let mut c0: u8  = 0;
+    let img = qoi::load("IMG-20260127-WA0001.qoi", &mut w0, &mut h0, &mut d0, &mut c0).unwrap();
 
-    let mut xt = vec![Complex::zero(); w as usize * h as usize];
-    for i in 0..h as usize {
-        for j in 0..w as usize {
-            xt[i * w as usize + j] = Complex::new(pix[(i * w as usize + j) * 3 + 0] as f32 / 255.0 * 2.0 - 1.0, 0.0);
+    // Kernel
+    let mut w1: u32 = 0;
+    let mut h1: u32 = 0;
+    let mut d1: u8  = 0;
+    let mut c1: u8  = 0;
+    let ker = qoi::load("flower.qoi", &mut w1, &mut h1, &mut d1, &mut c1).unwrap();
+
+    // FFT size
+    let w2: usize = w0 as usize + w1 as usize;
+    let h2: usize = h0 as usize + h1 as usize;
+
+    // Image to Complex
+    let mut rt0 = vec![Complex::zero(); w2 * h2];
+    let mut gt0 = vec![Complex::zero(); w2 * h2];
+    let mut bt0 = vec![Complex::zero(); w2 * h2];
+    for i in 0..h0 as usize {
+        for j in 0..w0 as usize {
+            let r = img[(i * w0 as usize + j) * d0 as usize + 0] as f32 / 255.0;
+            let g = img[(i * w0 as usize + j) * d0 as usize + 1] as f32 / 255.0;
+            let b = img[(i * w0 as usize + j) * d0 as usize + 2] as f32 / 255.0;
+            let a = if d0 > 3 { img[(i * w0 as usize + j) * d0 as usize + 3] as f32 / 255.0 } else { 1.0 };
+
+            rt0[i * w2 + j] = Complex::new((1.0 - r) * a, 0.0);
+            gt0[i * w2 + j] = Complex::new((1.0 - g) * a, 0.0);
+            bt0[i * w2 + j] = Complex::new((1.0 - b) * a, 0.0);
         }
     }
 
-    let xf = fft::fft2(&xt, w as usize, h as usize, 2);
-
-
-    let mut min_a = INFINITY;
-    let mut max_a = NEG_INFINITY;
-    for i in 0..h as usize {
-        for j in 0..w as usize {
-            let f = xf[i * w as usize + j];
-            let a = f32::sqrt(f.re * f.re + f.im * f.im);
-            min_a = f32::min(a, min_a);
-            max_a = f32::max(a, max_a);
+    // Kernel to complex
+    let mut rt1 = vec![Complex::zero(); w2 * h2];
+    let mut gt1 = vec![Complex::zero(); w2 * h2];
+    let mut bt1 = vec![Complex::zero(); w2 * h2];
+    for i in 0..h1 as usize {
+        for j in 0..w1 as usize {
+            rt1[i * w2 + j] = Complex::new(ker[(i * w1 as usize + j) * d1 as usize + 0] as f32 / 255.0, 0.0);
+            gt1[i * w2 + j] = Complex::new(ker[(i * w1 as usize + j) * d1 as usize + 1] as f32 / 255.0, 0.0);
+            bt1[i * w2 + j] = Complex::new(ker[(i * w1 as usize + j) * d1 as usize + 2] as f32 / 255.0, 0.0);
         }
     }
 
-    for i in 0..h as usize {
-        for j in 0..w as usize {
-            let iw = (i + (h as usize + 1) / 2) % h as usize;
-            let jw = (j + (w as usize + 1) / 2) % w as usize;
-            let f = xf[iw * w as usize + jw];
+    // Image FFT
+    let rf0 = fft::fft2(&rt0, w2, h2, 2);
+    let gf0 = fft::fft2(&gt0, w2, h2, 2);
+    let bf0 = fft::fft2(&bt0, w2, h2, 2);
 
-            // pix[(i * w as usize + j) * 3 + 0] = ((f.re - min_a) / (max_a - min_a) * 255.0) as u8;
-            // pix[(i * w as usize + j) * 3 + 1] = ((f.im - min_a) / (max_a - min_a) * 255.0) as u8;
-            // pix[(i * w as usize + j) * 3 + 2] = 0;
+    // Kernel FFT
+    let rf1 = fft::fft2(&rt1, w2, h2, 2);
+    let gf1 = fft::fft2(&gt1, w2, h2, 2);
+    let bf1 = fft::fft2(&bt1, w2, h2, 2);
 
-            let a = (f32::sqrt(f.re * f.re + f.im * f.im) - min_a) / (max_a - min_a) * 255.0;
-            pix[(i * w as usize + j) * 3 + 0] = a as u8;
-            pix[(i * w as usize + j) * 3 + 1] = a as u8;
-            pix[(i * w as usize + j) * 3 + 2] = a as u8;
+    let w2: usize = next_pow2(w2 as usize);
+    let h2: usize = next_pow2(h2 as usize);
+
+    let mut rf2 = vec![Complex::zero(); w2 * h2];
+    let mut gf2 = vec![Complex::zero(); w2 * h2];
+    let mut bf2 = vec![Complex::zero(); w2 * h2];
+
+    // Convolution
+    for i in 0..h2 as usize {
+        for j in 0..w2 as usize {
+            rf2[i * w2 + j] = rf0[i * w2 + j] * rf1[i * w2 + j];
+            gf2[i * w2 + j] = gf0[i * w2 + j] * gf1[i * w2 + j];
+            bf2[i * w2 + j] = bf0[i * w2 + j] * bf1[i * w2 + j];
         }
     }
 
-    qoi::save("fft2.qoi", w, h, d, c, &pix).unwrap();
+    // IFFT
+    let rt2 = fft::ifft2(&rf2, w2, h2, 2);
+    let gt2 = fft::ifft2(&gf2, w2, h2, 2);
+    let bt2 = fft::ifft2(&bf2, w2, h2, 2);
+
+    let rts = rt1.iter().cloned().reduce(|a, b| a + b).unwrap();
+    let gts = gt1.iter().cloned().reduce(|a, b| a + b).unwrap();
+    let bts = bt1.iter().cloned().reduce(|a, b| a + b).unwrap();
+
+    // let w0: u32 = w2 as u32;
+    // let h0: u32 = h2 as u32;
+
+    // Result
+    let mut pix = vec![0u8; w0 as usize * h0 as usize * 3];
+    for i in 0..h0 as usize {
+        for j in 0..w0 as usize {
+            let r = rt2[(i + h1 as usize / 2) * w2 + (j + w1 as usize / 2)];
+            let g = gt2[(i + h1 as usize / 2) * w2 + (j + w1 as usize / 2)];
+            let b = bt2[(i + h1 as usize / 2) * w2 + (j + w1 as usize / 2)];
+
+            let r = r.re / rts.re;
+            let g = g.re / gts.re;
+            let b = b.re / bts.re;
+
+            let r = r * 255.0;
+            let g = g * 255.0;
+            let b = b * 255.0;
+
+            // let r = r.pow(1.0 / 2.2);
+            // let g = g.pow(1.0 / 2.2);
+            // let b = b.pow(1.0 / 2.2);
+
+            // let r = r.re * r.re + r.im * r.im;
+            // let g = g.re * g.re + g.im * g.im;
+            // let b = b.re * b.re + b.im * b.im;
+
+            pix[(i * w0 as usize + j) * 3 + 0] = r as u8;
+            pix[(i * w0 as usize + j) * 3 + 1] = g as u8;
+            pix[(i * w0 as usize + j) * 3 + 2] = b as u8;
+        }
+    }
+
+    qoi::save("IMG-20260127-WA0001_fft.qoi", w0, h0, 3, 0, &pix).unwrap();
 }
